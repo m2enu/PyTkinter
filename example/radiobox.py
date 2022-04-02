4d9caa6e4ac2650178c1b5b9b7c69cb3bedcc82e
m2enu <m2enu915vogel@gmail.com>
2022-04-02 17:29:13 +0900

    update radiobox example to use ttk

--- example/radiobox.py
+++ example/radiobox.py
@@ -1,15 +1,24 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 
 if (__name__ == '__main__'):
     root = tk.Tk()
     root.title("PyTkinter")
-    root.geometry("300x400")
+    root.geometry("300x500")
 
-    val = tk.IntVar(value=1)
-    radioboxes = (tk.Radiobutton(
-        master=root, text="Item"+chr(0x31+i), value=i, variable=val, command=lambda:print(val.get()))
+    tkval = tk.IntVar(value=1)
+    tkradio = (tk.Radiobutton(
+        master=root, text="Item"+chr(0x31+i), value=i, variable=tkval, command=lambda:print(tkval.get()))
         for i in range(4))
-    for radio in radioboxes:
-        radio.pack(expand=True)
+    for radio in tkradio:
+        radio.pack(pady=4)
+
+    ttkval = tk.IntVar(value=1)
+    ttkradio = (ttk.Radiobutton(
+        master=root, text="Item"+chr(0x41+i), value=i, variable=ttkval)
+        for i in range(4))
+    for radio in ttkradio:
+        radio.bind("<ButtonRelease>", lambda e: print(e, ttkval.get()))
+        radio.pack(pady=4)
 
     root.mainloop()
