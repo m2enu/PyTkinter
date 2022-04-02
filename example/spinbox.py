e6a9194de69e5c9e3191000051dc3f4b25af40cf
m2enu <m2enu915vogel@gmail.com>
2022-04-02 17:19:06 +0900

    update spinbox example to use ttk

--- example/spinbox.py
+++ example/spinbox.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 
 if (__name__ == '__main__'):
     root = tk.Tk()
@@ -7,17 +8,22 @@ if (__name__ == '__main__'):
 
     # Integer
     spinint = tk.Spinbox(master=root, state="readonly", from_=0, to_=10, increment=1)
-    spinint.pack(expand=True)
+    spinint.pack(pady=8)
 
     # Float
     spinflt = tk.Spinbox(master=root, state="readonly", from_=0, to_=1, increment=0.1, format="%3.1f")
-    spinflt.pack(expand=True)
+    spinflt.pack(pady=8)
 
     # with StringVar
     val = tk.StringVar()
     val.set("Bob")
     members = ("Alice", "Bob", "Carol")
     spinstr = tk.Spinbox(master=root, state="readonly", textvariable=val, values=members)
-    spinstr.pack(expand=True)
+    spinstr.pack(pady=8)
+
+    # Integer via ttk
+    ttkspinint = ttk.Spinbox(master=root, state="readonly", from_=0, to_=10, increment=1)
+    ttkspinint.set(1)
+    ttkspinint.pack(pady=8)
 
     root.mainloop()
