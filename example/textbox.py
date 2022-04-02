aa4c748d16913c2c7db360b37cfbf7a6685157ad
m2enu <m2enu915vogel@gmail.com>
2022-04-02 17:15:20 +0900

    update textbox example to use ttk

--- example/textbox.py
+++ example/textbox.py
@@ -1,17 +1,23 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 
 if (__name__ == '__main__'):
     root = tk.Tk()
     root.title("PyTkinter")
     root.geometry("400x600")
 
-    # Single line
-    tbsingle = tk.Entry(master=root)
-    tbsingle.insert(0, "hello world !")
-    tbsingle.pack(pady=10)
+    # Single line via tk
+    tktxts = tk.Entry(master=root)
+    tktxts.insert(0, "hello tk world !")
+    tktxts.pack(pady=8)
 
-    # Multi lines
-    tbmulti = tk.Text(master=root, height=10)
-    tbmulti.pack(pady=10)
+    # Single line via ttk
+    ttktxts = ttk.Entry(master=root)
+    ttktxts.insert(0, "hello ttk world !")
+    ttktxts.pack(pady=8)
+
+    # Multi lines via tk
+    tktxtm = tk.Text(master=root, height=8)
+    tktxtm.pack(pady=8)
 
     root.mainloop()
