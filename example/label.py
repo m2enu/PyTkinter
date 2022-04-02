a222deee2302553bf3927d849ff7300cd7d86421
m2enu <m2enu915vogel@gmail.com>
2022-04-02 16:42:51 +0900

    update label example to use ttk

--- example/label.py
+++ example/label.py
@@ -1,11 +1,15 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 
 if (__name__ == '__main__'):
     root = tk.Tk()
     root.title("PyTkinter")
     root.geometry("400x300")
 
-    label = tk.Label(master=root, text="hello world !")
-    label.pack()
+    lbltk = tk.Label(master=root, text="hello tk world !")
+    lbltk.pack(pady=8)
+
+    lblttk = ttk.Label(master=root, text="hello ttk world !")
+    lblttk.pack(pady=8)
 
     root.mainloop()
